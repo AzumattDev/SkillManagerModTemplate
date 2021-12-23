725f9c0c66274d454d2d96b94c853794798692af
AzumattDev <azumattgaming@gmail.com>
2021-12-23 18:16:28 -0500

    Init

---
+++ Plugin.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using JetBrains.Annotations;
+using SkillManager;
+using ServerSync;
+using UnityEngine;
+
+namespace SkillManagerModTemplate
+{
+    /* Please note, by default this template uses the class and not the DLL load for Skill Manager.
+     Follow the instructions in the README.md file and the ILRepack.targets file to switch over to using the DLL if you wish. */
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class SkillManagerModTemplatePlugin : BaseUnityPlugin
+    {
+        internal const string ModName = "SkillManagerModTemplate";
+        internal const string ModVersion = "1.0.0";
+        internal const string Author = "azumatt";
+        private const string ModGUID = Author + "." + ModName;
+        private static string ConfigFileName = ModGUID + ".cfg";
+        private static string ConfigFileFullPath = Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName;
+        internal static string ConnectionError = "";
+
+        public static readonly ManualLogSource SkillManagerModTemplateLogger =
+            BepInEx.Logging.Logger.CreateLogSource(ModName);
+
+        private static readonly ConfigSync ConfigSync = new(ModGUID)
+            { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
+
+        public void Awake()
+        {
+            _serverConfigLocked = config("General", "Force Server Config", true, "Force Server Config");
+            _ = ConfigSync.AddLockingConfigEntry(_serverConfigLocked);
+
+            Skill
+                tenacity = new("Tenacity",
+                    "tenacity-icon.png"); // Skill name along with the skill icon. By default the icon is found in the icons folder. Put it there if you wish to load one.
+
+            tenacity.Description.English("Reduces damage taken by 0.2% per level.");
+            tenacity.Name.German("Hartnäckigkeit"); // Use this to localize values for the name
+            tenacity.Description.German(
+                "Reduziert erlittenen Schaden um 0,2% pro Stufe."); // You can do the same for the description
+            tenacity.Configurable = true;
+
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Harmony harmony = new(ModGUID);
+            harmony.PatchAll(assembly);
+            SetupWatcher();
+        }
+
+        private void OnDestroy()
+        {
+            Config.Save();
+        }
+
+        private void SetupWatcher()
+        {
+            FileSystemWatcher watcher = new(Paths.ConfigPath, ConfigFileName);
+            watcher.Changed += ReadConfigValues;
+            watcher.Created += ReadConfigValues;
+            watcher.Renamed += ReadConfigValues;
+            watcher.IncludeSubdirectories = true;
+            watcher.SynchronizingObject = ThreadingHelper.SynchronizingObject;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void ReadConfigValues(object sender, FileSystemEventArgs e)
+        {
+            if (!File.Exists(ConfigFileFullPath)) return;
+            try
+            {
+                SkillManagerModTemplateLogger.LogDebug("ReadConfigValues called");
+                Config.Reload();
+            }
+            catch
+            {
+                SkillManagerModTemplateLogger.LogError($"There was an issue loading your {ConfigFileName}");
+                SkillManagerModTemplateLogger.LogError("Please check your config entries for spelling and format!");
+            }
+        }
+
+
+        #region ConfigOptions
+
+        private static ConfigEntry<bool>? _serverConfigLocked;
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description,
+            bool synchronizedSetting = true)
+        {
+            ConfigDescription extendedDescription =
+                new(
+                    description.Description +
+                    (synchronizedSetting ? " [Synced with Server]" : " [Not Synced with Server]"),
+                    description.AcceptableValues, description.Tags);
+            ConfigEntry<T> configEntry = Config.Bind(group, name, value, extendedDescription);
+            //var configEntry = Config.Bind(group, name, value, description);
+
+            SyncedConfigEntry<T> syncedConfigEntry = ConfigSync.AddConfigEntry(configEntry);
+            syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
+
+            return configEntry;
+        }
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, string description,
+            bool synchronizedSetting = true)
+        {
+            return config(group, name, value, new ConfigDescription(description), synchronizedSetting);
+        }
+
+        private class ConfigurationManagerAttributes
+        {
+            public bool? Browsable = false;
+        }
+
+        #endregion
+    }
+}
---
+++ README.md
@@ -0,0 +1,53 @@
+﻿# Skill Manager Mod Template
+
+Some of the steps needed to set up a project using the Skill Manager from Blaxxun have already been done for you. Please read this file carefully if you have any issues. Also, asking in the OdinPlus discord if you need further assistance.
+
+# Skill Manager
+
+Can be used to easily add new skills to Valheim. Will automatically add config options to your mod and sync the configuration from a server, if the mod is installed on the server as well.
+Please note that you still have to implement the effect your skill has yourself.
+
+## How to add skills
+
+Copy the skill icon (a 64x64 image) into an `icons` directory in your project and make sure to set it as an EmbeddedResource in the properties of the asset bundle.
+
+### Merging the DLLs into your mod
+
+The below steps are already done for you to merge ServerSync.dll into your mod. If you wish to not use the class for SkillManager please either remove or comment out the class code. Then navigate to the ILRepack.targets file and uncomment out the line to add the SkillManager.dll to the merged dlls.
+
+Including the DLLs is best done via ILRepack (https://github.com/ravibpatel/ILRepack.Lib.MSBuild.Task). You can load this package (ILRepack.Lib.MSBuild.Task) from NuGet.
+
+The template should have installed ILRepack via NuGet, simply create/edit the file named `ILRepack.targets` in your project and copy the following content into the file. This file should already be a part of the template. 
+
+```xml
+<?xml version="1.0" encoding="utf-8"?>
+<Project xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+    <Target Name="ILRepacker" AfterTargets="Build">
+        <ItemGroup>
+            <InputAssemblies Include="$(TargetPath)" />
+            <InputAssemblies Include="$(OutputPath)\SkillManager.dll" />
+            <InputAssemblies Include="$(OutputPath)\ServerSync.dll" />
+        </ItemGroup>
+        <ILRepack Parallel="true" DebugInfo="true" Internalize="true" InputAssemblies="@(InputAssemblies)" OutputFile="$(TargetPath)" TargetKind="SameAsPrimaryAssembly" LibraryPath="$(OutputPath)" />
+    </Target>
+</Project>
+```
+
+Make sure to set the SkillManager.dll and the ServerSync.dll in your project to "Copy to output directory" in the properties of the DLLs and to add a reference to it. If you are not using the SkillManager.dll and are instead, using the class, you do not need to set this value for that DLL.
+After that, simply add `using SkillManager;` to your mod and use the `Skill` class, to add your skills.
+
+The SkillManager also adds two extension methods `GetSkillFactor` and `RaiseSkill` on `Character` and `Skills` classes to get and increase the skill in your code by skill name.
+
+## Example projects
+
+https://github.com/blaxxun-boop/Tenacity
+
+This adds a skill `Tenacity`, which reduces the damage taken slightly. The `tenacity-icon.png` file is in a directory called `icons`.
+
+https://github.com/blaxxun-boop/Building
+
+This adds a skill `Building`, which increases the health of pieces built by you.
+
+https://github.com/blaxxun-boop/Evasion
+
+This adds a skill `Evasion`, which reduces the stamina usage of dodging.
